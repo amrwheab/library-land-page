1c8fea1bd126f7d378c8168d0861dfa3c926531a
amrwheab <amr.wheab2020@gmail.com>
2021-07-13 02:17:02 +0200

    first commit

---
+++ css/own.js
@@ -0,0 +1,8 @@
+function dep()
+{
+    document.getElementById("sortt").style="transform: scale(1,1);";
+}
+function dep2()
+{
+    document.getElementById("sortt").style="transform: scale(1,0);";
+}
